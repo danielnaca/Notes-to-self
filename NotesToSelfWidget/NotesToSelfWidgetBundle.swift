1ffd36ae38b0ff9087c2f1451fc1a97aa38ed010
Daniel Nacamuli <cursive@users.noreply.github.com>
2025-06-14 19:08:34 -0700

    First version, adding items shows them in a list and in the widget

---
+++ NotesToSelfWidget/NotesToSelfWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  NotesToSelfWidgetBundle.swift
+//  NotesToSelfWidget
+//
+//  Created by Daniel Nacamuli on 6/14/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NotesToSelfWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NotesToSelfWidget()
+        NotesToSelfWidgetControl()
+    }
+}
